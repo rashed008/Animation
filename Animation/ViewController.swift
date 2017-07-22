1c664710b0ed3f91a0955a7d9f9946b95bb0896a
RASHED <hello@MacBooks-MacBook-Pro.local>
2017-07-22 12:39:36 +0600

    fadeIn SlideIn grow

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -13,6 +13,39 @@ class ViewController: UIViewController {
     @IBOutlet var button: UIButton!
     @IBOutlet var image: UIImageView!
     
+    @IBAction func fadeIn(_ sender: Any) {
+        
+        image.alpha = 0
+        UIView.animate(withDuration: 1, animations: {
+        
+         self.image.alpha = 1
+            
+        })
+        
+    }
+    
+    @IBAction func slideIn(_ sender: Any) {
+        
+        //CG means core graphics
+        image.center = CGPoint(x: image.center.x - 500, y: image.center.y)
+        UIView.animate(withDuration : 2){
+            self.image.center = CGPoint(x: self.image.center.x + 500, y: self.image.center.y)
+        
+        }
+        
+    }
+    
+    
+    @IBAction func grow(_ sender: Any) {
+        
+        image.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        
+        UIView.animate(withDuration: 1){
+            
+            self.image.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        }
+        
+    }
       var counter = 1
       var isAnimating = false
       var timer = Timer()
